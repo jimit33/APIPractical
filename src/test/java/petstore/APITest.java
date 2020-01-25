4bf7ed5540af365a0c64835b26dfdd5dd3073798
jimit333 <jimit33@gmail.com>
2020-01-25 16:10:36 +0000

    Updates

--- src/test/java/petstore/APITest.java
+++ src/test/java/petstore/APITest.java
@@ -9,47 +9,52 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 public class APITest {
-    int id = 12345;
-    String jsonData = "{\"id\":" + this.id + ",\"category\":{\"id\":1,\"name\":\"domestic\"},\"name\":\"GoodDoggo\",\"photoUrls\":[\"urlOfDogPic\"],\"tags\":[{\"id\":1,\"name\":\"tag1\"}],\"status\":\"available\"}";
+    private static final int ID = 12345;
+    private  static final String BASE_URI = "https://petstore.swagger.io";
+    String jsonData = "{\"id\":" + ID + ",\"category\":{\"id\":1,\"name\":\"domestic\"},\"name\":\"GoodDoggo\",\"photoUrls\":[\"urlOfDogPic\"],\"tags\":[{\"id\":1,\"name\":\"tag1\"}],\"status\":\"available\"}";
 
     @Test
     public void task1CreatePet(){
-        System.out.println("CreatePet executed");
-
-        RestAssured.baseURI= "https://petstore.swagger.io";
-
+        /* Creating new Pet entry using Post request
+        *  hardcoded body data from defined variable "jasonData" is used to create new entry */
+        RestAssured.baseURI= BASE_URI;
         given().contentType(ContentType.JSON).body(jsonData).when().post("/v2/pet").then().assertThat().statusCode(200);
+        /* Asserted StatusCode = 200 to verify Pet entry is created */
+        System.out.println("CreatePet executed");
     }
 
     @Test
     public void task2VerifyData(){
-        System.out.println("Verify Pet Created with Correct Data");
-        RestAssured.baseURI= "https://petstore.swagger.io";
+        /* Verify data of the Pet which was created
+        *  Using Get method to search Pet using ID and
+        *  comparing data with "jasonData" variable used to create pet */
+        RestAssured.baseURI=  BASE_URI;
         Response response;
         response = RestAssured.given().urlEncodingEnabled(true)
-                .get("/v2/pet/" + this.id);
+                .get("/v2/pet/" + ID);
+        /* Assert if Response data equals to data used to create Pet */
         Assert.assertTrue(response.asString().equals(jsonData),"Response not matching with data used to create Pet");
+        System.out.println("Verify Pet Created with Correct Data");
     }
 
     @Test
     public void task3UpdatePet(){
-        System.out.println("UpdatePet Executed");
-
-        RestAssured.baseURI= "https://petstore.swagger.io";
-
+        /* Updating a Pet_Name in store using form data - Post request */
+        RestAssured.baseURI=  BASE_URI;
         given().urlEncodingEnabled(true)
                 .param("name","VeryGoodDoggo")
-                .post("/v2/pet/" + this.id)
-                .then().assertThat().statusCode(200);
+                .post("/v2/pet/" + ID)
+                .then().assertThat().statusCode(200); // Asserting status code to verify Update
+        System.out.println("UpdatePet Executed");
     }
 
     @Test
     public void task4DeletePet(){
-        System.out.println("DeletePet Executed");
-
-        RestAssured.baseURI= "https://petstore.swagger.io";
-
+        /* Deleting the Pet which was created and updated in earlier test cases */
+        RestAssured.baseURI=  BASE_URI;
         given().urlEncodingEnabled(true)
-                .delete("/v2/pet/" + this.id).then().assertThat().statusCode(200);
+                .delete("/v2/pet/" + ID)
+                .then().assertThat().statusCode(200); // Asserting status code to verify record Deleted
+        System.out.println("DeletePet Executed");
     }
 }
